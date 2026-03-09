9a9f303116d16c934af17687368b7e1d65d3eeb0
snehasiskhan2000-source <snehasiskhan2000@gmail.com>
2026-03-09 18:24:32 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Install Python packages
-pip install -r requirements.txt
-
-# Install Playwright browser and its system dependencies
-playwright install chromium
-playwright install-deps
